d4cfe7f754e77b47cefd0e49400ba2360635b815
Nino Floris <mail@ninofloris.com>
2022-01-24 16:48:43 +0100

    Missing constraint info repro

--- console/Program.cs
+++ console/Program.cs
@@ -1,2 +1,12 @@
 ﻿// Cannot access internal constructor 'X' here
 new lib.X();
+
+class Foo : lib.IFoo
+{
+    // The constraints for type parameter 'T' of method 'Foo.Invoke<T>' must match the constraints for type parameter 'T'
+    // of interface method 'lib.IFoo.Invoke<T>'. Consider using an explicit interface implementation instead.
+    public void Invoke<T>(T value)
+        where T : ICloneable
+    {
+    }
+}
--- lib/Library.fs
+++ lib/Library.fs
@@ -1,7 +1,11 @@
 ﻿namespace lib
 
+open System
 open System.Runtime.CompilerServices
 [<assembly: InternalsVisibleTo("console")>]
 do ()
 
 type internal X() = class end
+
+type IFoo =
+    abstract Invoke<'T when 'T :> ICloneable> : value: 'T -> Unit
